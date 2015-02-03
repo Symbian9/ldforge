91a756541d49c2c6e72f3c43729a4ad468a7ba15
Teemu Piippo <crimsondusk64@gmail.com>
2015-02-03 15:30:30 +0200

    - added/corrected license headers

--- src/script/parser.h
+++ src/script/parser.h
@@ -1,6 +1,6 @@
 /*
  *  LDForge: LDraw parts authoring CAD
- *  Copyright (C) 2013 - 2015 Teemu Piippo
+ *  Copyright (C) 2015 Teemu Piippo
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
