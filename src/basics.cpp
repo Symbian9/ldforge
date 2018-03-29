f3f75d3a4e169ff475d4a660ab9526b3f8a33a57
Teemu Piippo <teemu@hecknology.net>
2018-03-29 10:18:18 +0300

    use 3 significant figures in file size representation

--- src/basics.cpp
+++ src/basics.cpp
@@ -73,7 +73,7 @@ QString formatFileSize(qint64 size)
 	static const QString suffixes[] = {" bytes", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
 	int magnitude = (size > 0) ? floor(log10(size) / 3.0 + 1e-10) : 0;
 	magnitude = qBound(0, magnitude, countof(suffixes) - 1);
-	return QString::number(size / pow(1000, magnitude)) + suffixes[magnitude];
+	return QString::number(size / pow(1000, magnitude), 'f', 3) + suffixes[magnitude];
 }
 
 /*
